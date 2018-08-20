fd50189cabf05e531daaea018c513112d1bfcae6
Mouad <mouad.khchich@gmail.com>
2018-08-20 14:59:53 +0100

    Added cookie management

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Loading modules
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 
 // Setting up the app
@@ -8,6 +10,15 @@ const app = express();
 
 app.set('view engine','ejs');
 app.use('/assets',express.static('assets'));
+
+// Set up the session
+app.use(session({
+  name: 'cookie-name',
+  secret: 'shhht',
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(cookieParser());
 
 // Loading the controller
 const Articles = require('./controllers/Articles');
--- controllers/articles.js
+++ controllers/articles.js
@@ -1,6 +1,5 @@
 // Loading the modules
 const bodyParser = require('body-parser');
-const session = require('express-session');
 const db = require('./db');
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -8,134 +7,138 @@ let urlencodedParser = bodyParser.urlencoded({ extended: false });
 // Exporting the whole class
 module.exports = class Articles{
 
-  constructor(app) {
-    // Connecting to the database
-    db.connect();
+constructor(app) {
+// Connecting to the database
+db.connect();
 
-    // Set up the session
-    app.use(session({
-      name: 'cookie-name',
-      secret: 'shhht',
-      saveUninitialized: true,
-      resave: true
-    }));
+app.use((req,res,next)=>{
+  if(!req.session.username){
+    if(req.cookies.username){
+      req.session.username = req.cookies.username;
+    }
+  }
+  next();
+});
 
-    app.use((req,res,next)=>{
-      // req.session.username = "Mouad";
-      next();
+// Setting the routes
+app.get('/',(req,res) => {
+  db.articleModel.find({},(err,result)=>{
+    if(err) throw err;
+    res.render('home',{
+      session:req.session,
+      articles:result
     });
+  });
+});
 
-    // Setting the routes
-    app.get('/',(req,res) => {
-      db.articleModel.find({},(err,result)=>{
-        if(err) throw err;
-        res.render('home',{
-          session:req.session,
-          articles:result
-        });
-      });
-    });
-
-    app.get('/article/:title',(req,res) => {
-      let title = req.params.title;
-      title = title.replace(/\s+/g, '-').toLowerCase();
-      if(title=='new'){
-        res.render('article-new',{session:req.session});
-        console.log(req.session.id);
-      }else{
-        db.articleModel.find({},(err,result)=>{
-          if (err) throw err;
-          let found = false;
-          result.forEach((el)=>{
-            let tmp = el.title;
-            tmp = tmp.replace(/\s+/g, '-').toLowerCase();
-            if (tmp == title) {
-              res.render('article',{session:req.session,data:el});
-              found = true;
-            }
-          });
-          if(!found) res.render('404');
-        });
-      }
-    });
-
-    app.post('/article',urlencodedParser,(req,res) => {
-      if (!req.body) return res.sendStatus(400);
-      let today = new Date();
-      let dd = today.getDate();
-      let mm = today.getMonth()+1; //January is 0!
-      let yyyy = today.getFullYear();
-
-      if(dd<10) {
-        dd = '0'+dd
-      }
-
-      if(mm<10) {
-        mm = '0'+mm
-      }
-
-      today = dd + '-' + mm + '-' + yyyy;
-      let data = {
-        title: req.body.title,
-        article:req.body.article,
-        author:req.body.author,
-        views: 0,
-        date: today,
-        image: "https://via.placeholder.com/200x200"
-      }
-      db.addArticle(data);
-      res.send("200");
-    });
-
-    app.get('/login',(req,res) => {
-      if (req.session.email) {
-        res.redirect('/');
-      }else{
-        res.render('login',{session:req.session});
-      }
-    });
-    
-    app.get('/signup',(req,res) => {
-      if (req.session.email) {
-        res.redirect('/');
-      }else{
-        res.render('signup',{session:req.session});
-      }
-    });
-    
-    app.post('/login',urlencodedParser,(req,res) => {
-      let data = req.body;
-      db.userModel.findOne({email:data.email}, (err,user) => {
-        if(err) throw err;
-        if(user && user.password === data.password){
-          req.session.email = user.email;
-          req.session.username = user.username;
-          req.session.level = user.level;
-          res.redirect('/');
-        }else{
-          res.render('login',{error:true});
+app.get('/article/:title',(req,res) => {
+  let title = req.params.title;
+  title = title.replace(/\s+/g, '-').toLowerCase();
+  if(title=='new'){
+    res.render('article-new',{session:req.session});
+    console.log(req.session.id);
+  }else{
+    db.articleModel.find({},(err,result)=>{
+      if (err) throw err;
+      let found = false;
+      result.forEach((el)=>{
+        let tmp = el.title;
+        tmp = tmp.replace(/\s+/g, '-').toLowerCase();
+        if (tmp == title) {
+          res.render('article',{session:req.session,data:el});
+          found = true;
         }
       });
-    });
-
-    app.post('/signup',urlencodedParser ,(req,res) => {
-      let tmp = new db.userModel({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-      });
-      tmp.save((err)=>{
-        if(err) throw err;
-        req.session.email = req.body.email;
-        req.session.username = req.body.username;
-        req.session.level = "reader";
-        res.redirect('/');
-      });
-    });
-
-    app.get('/logout',(req,res) => {
-      if(req.session.username) req.session.destroy();
-      res.redirect(req.get('Referrer'));
+      if(!found) res.render('404');
     });
   }
+});
+
+app.post('/article',urlencodedParser,(req,res) => {
+  if (!req.body) return res.sendStatus(400);
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth()+1; //January is 0!
+  let yyyy = today.getFullYear();
+
+  if(dd<10) {
+    dd = '0'+dd
+  }
+
+  if(mm<10) {
+    mm = '0'+mm
+  }
+
+  today = dd + '-' + mm + '-' + yyyy;
+  let data = {
+    title: req.body.title,
+    article:req.body.article,
+    author:req.body.author,
+    views: 0,
+    date: today,
+    image: "https://via.placeholder.com/200x200"
+  }
+  db.addArticle(data);
+  res.send("200");
+});
+
+app.get('/login',(req,res) => {
+  if (req.session.email) {
+    res.redirect('/');
+  }else{
+    res.render('login',{session:req.session});
+  }
+});
+
+app.get('/signup',(req,res) => {
+  if (req.session.email) {
+    res.redirect('/');
+  }else{
+    res.render('signup',{session:req.session});
+  }
+});
+
+app.post('/login',urlencodedParser,(req,res) => {
+  let data = req.body;
+  console.log(data);
+  db.userModel.findOne({email:data.email}, (err,user) => {
+    if(err) throw err;
+    if(user && user.password === data.password){
+      req.session.email = user.email;
+      req.session.username = user.username;
+      req.session.level = user.level;
+      if(data.stay === 'on'){
+        res.cookie("username",user.username,{
+          maxAge:1000 * 60 * 60 * 24,
+          httpOnly: true
+        });
+      }
+      res.redirect('/');
+    }else{
+      res.render('login',{error:true});
+    }
+  });
+});
+
+app.post('/signup',urlencodedParser ,(req,res) => {
+  let tmp = new db.userModel({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+  tmp.save((err)=>{
+    if(err) throw err;
+    req.session.email = req.body.email;
+    req.session.username = req.body.username;
+    req.session.level = "reader";
+    res.redirect('/');
+  });
+});
+
+app.get('/logout',(req,res) => {
+  if(req.session.username) req.session.destroy();
+  if(req.cookies.username) res.clearCookie("username");
+  res.redirect(req.get('Referrer'));
+});
+}
 }
